8f0d2a2d896b9552b19e5b31fdb03b33a71c7170
Ross Gaskell <ross.gaskell@dinersclub.co.uk>
2017-12-15 17:09:45 +0000

    - Initial Commit of Interest Sliders

--- prototype/js/app.js
+++ prototype/js/app.js
@@ -14,6 +14,62 @@ $(document).ready(function () {
 		toggleFixedNav(nav);
 	});
 
+
+	// Construct Sliders
+	
+	var interestSlider = $(".interest-slider");
+
+	$(interestSlider).submit(function(e) {
+		$(this).find('input[name=amount]').val(
+			$( "#amount-slider" ).slider( "value" )
+		);
+		$(this).find('input[name=term]').val(
+			$( "#term-slider" ).slider( "value" )
+		);
+	});
+
+	// If slider containter present initialise sliders
+	if (interestSlider.length > 0) {
+
+		var amountMin = parseInt($(interestSlider).attr('data-amount-min'));
+		var amountMax = parseInt($(interestSlider).attr('data-amount-max'));
+		var termMin = parseInt($(interestSlider).attr('data-term-min'));
+		var termMax = parseInt($(interestSlider).attr('data-term-max'));
+
+		// Pricing Sliders
+		$( "#amount-slider" ).slider({
+			range: "max",
+			min: amountMin,
+			max: amountMax,
+			step: 500,
+			value: 2000,
+			create: function() {
+				var text = "£" + $( this ).slider( "value" );
+				$( this ).find('.ui-slider-handle').text( text );
+			},
+			slide: function( event, ui ) {
+				var text = "£" + ui.value;
+				$( this ).find('.ui-slider-handle').text( text );
+			}
+		});
+
+		$( "#term-slider" ).slider({
+			range: "max",
+			min: termMin,
+			max: termMax,
+			value: 6,
+			create: function() {
+				var text = $( this ).slider( "value" ) + " months";
+				$( this ).find('.ui-slider-handle').text( text );
+			},
+			slide: function( event, ui ) {
+				var text = ui.value + " months";
+				$( this ).find('.ui-slider-handle').text( text );
+			}
+		});
+
+	}
+
 });
 
 // Toggle Navigation Fixed State 
